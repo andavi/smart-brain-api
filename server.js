5bad26b222886a17d9a1c713895291376041c2fd
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-05-07 21:11:43 -0400

    link database

--- server.js
+++ server.js
@@ -2,113 +2,95 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const db = require('knex')({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'andavi',
+    password : '',
+    database : 'smart-brain'
+  }
+});
 
 const app = express();
-
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'John',
-			email: 'john@email.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'Sally',
-			email: 'sally@email.com',
-			password: 'bananas',
-			entries: 0,
-			joined: new Date()
-		}
-	],
-	login: [
-		{
-			id: '987',
-			hash: '',
-			email: 'jay@email.com'
-		}
-	]
-}
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-	res.send(database.users);
 });
 
 app.post('/signin', (req, res) => {
-	const hash = '$2a$10$qnr9OjtHTxx3xXRgh9ejdOwH5v4KZXsmcpFfgc7WgSpN2/AhZYrLK';
-	// Load hash from your password DB.
-	bcrypt.compare("apples", hash, function(err, res) {
-	    console.log('first guess:', res);
-	});
-	bcrypt.compare("veggies", hash, function(err, res) {
-	    console.log('second guess:', res);
-	});
-	if (req.body.email === database.users[0].email && 
-		req.body.password === database.users[0].password) {
-		res.json(database.users[0]);
-	} else {
-		res.status(400).json('failure: error logging in');
-	}
+	db.select('email', 'hash').from('login')
+		.where('email', '=', req.body.email)
+		.then(data => {
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+			if (isValid) {
+				return db.select('*').from('users')
+					.where('email', '=', req.body.email)
+					.then(user => {
+						res.json(user[0]);
+					})
+					.catch(err => res.status(400).json('Unable to get user'));
+			} else {
+				res.status(400).json('Wrong credentials');
+			}
+		})
+		.catch(err => res.status(400).json('Wrong credentials'));
 });
 
 app.post('/register', (req, res) => {
 	const { email, name, password } = req.body;
-	bcrypt.hash(password, null, null, function(err, hash) {
-    	console.log(hash);
-	});
-	database.users.push({
-		id: '125',
-		name: name,
-		email: email,
-		entries: 0,
-		joined: new Date()
+	const hash = bcrypt.hashSync(password);
+	db.transaction(trx => {
+		trx.insert({
+			hash: hash,
+			email: email
+		})
+		.into('login')
+		.returning('email')
+		.then(loginEmail => {
+				return trx('users')
+				.returning('*')
+				.insert({
+					email: loginEmail[0],
+					name: name,
+					joined: new Date()
+				})
+				.then(user => {
+					res.json(user[0]);
+				})
+				.then(trx.commit)
+				.catch(trx.rollback);
+		})
 	})
-	res.json(database.users[database.users.length-1]);
+		.catch(err => res.status(400).json('Unable to register.'));
+	
 });
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
+	db.select('*').from('users').where({ id }).then(user => {
+		if (user.length) {
+			res.json(user[0]);
+		} else {
+			res.status(400).json('Not found')
 		}
-	});
-	if (!found) {
-		res.status(400).json('not found');
-	}
+	})
+	.catch(err => res.status(400).json('Error getting user'));
 });
 
 app.put('/image', (req, res) => {
 	const { id } = req.body;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			user.entries++;
-			return res.json(user.entries);
-		}
-	});
-	if (!found) {
-		res.status(400).json('not found');
-	}
+	db('users').where('id', '=', id)
+		.increment('entries', 1)
+		.returning('entries')
+		.then(entries => {
+			res.json(entries[0]);
+		})
+		.catch(err => res.status(400).json('Unable to get entries'));
 });
 
 app.listen(3000, () => {
 	console.log('App is running on port 3000.');
 });
-
-/*
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT = user
-*/
