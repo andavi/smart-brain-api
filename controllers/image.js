62939ac3825a52f9007f44a5c03a20157cba0c8b
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-05-07 23:34:24 -0400

    add clarifai api call

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'e0b0295af1b74d81be3ba345f2d743c3'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+      .then(data => {
+      	res.json(data);
+      })
+      .catch(err => res.status(400).json('Unable to access Claifai API'));
+  }
+
 const handleImage = (req, res, db) => {
 	const { id } = req.body;
 	db('users').where('id', '=', id)
@@ -10,5 +25,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-	handleImage
+	handleImage,
+	handleApiCall
 }
